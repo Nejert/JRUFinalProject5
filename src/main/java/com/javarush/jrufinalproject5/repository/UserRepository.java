aca64c82c6dff9f2c7b9f4abe70512e70dfc6564
Igor Kazakov <nejer777@gmail.com>
2026-01-14 17:28:54 +0600

    Added repository

---
+++ src/main/java/com/javarush/jrufinalproject5/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.javarush.jrufinalproject5.repository;
+
+
+import com.javarush.jrufinalproject5.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLogin(String login);
+}
